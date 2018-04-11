2d697b55bc3991385ccde78eff7e80e3aee852bc
kPherox <admin@mail.kr-kp.com>
2018-04-11 18:32:33 +0900

    Replace $depth -> $loop->depth

--- src/views/menu.blade.php
+++ src/views/menu.blade.php
@@ -6,7 +6,7 @@
                 <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                     {{ $name }}
                 </a>
-                @include('LaravelTopNav::menu_items', ['depth' => 0, 'items' => $path])
+                @include('LaravelTopNav::menu_items', ['items' => $path])
             </li>
         <?php else: ?>
             <li class="nav-item"><a class="nav-link" href="{{ $path }}">{{ trans('LaravelTopNav::ui.top_nav.'.$name) }}</a></li>
--- src/views/menu_items.blade.php
+++ src/views/menu_items.blade.php
@@ -1,13 +1,12 @@
 <ul class="dropdown-menu" role="menu">
-    <?php $depth++; ?>
     @foreach($items as $name => $path)
         <?php if(is_array($path)): ?>
-            @if($depth + 1 <= config('top_nav.max_depth'))
+            @if($loop->depth <= config('top_nav.max_depth'))
                 <li class="dropdown-submenu nav-item">
                     <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                         {{ $name }}
                     </a>
-                    @include('LaravelTopNav::menu_items', ['depth' => $depth, 'items' => $path])
+                    @include('LaravelTopNav::menu_items', ['items' => $path])
                 </li>
             @endif
         <?php else: ?>
