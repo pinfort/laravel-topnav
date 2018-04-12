42900f09315ed37adaf5743af8599f8fea04fb32
kPherox <admin@mail.kr-kp.com>
2018-04-12 18:27:35 +0900

    Add @topnavlang() directive

--- src/TopNavServiceProvider.php
+++ src/TopNavServiceProvider.php
@@ -35,6 +35,12 @@ class TopNavServiceProvider extends ServiceProvider
             __DIR__.'/assets' => public_path('vendor/TopNav'),
         ], 'public');
 
+        // translate directive
+        Blade::directive('topnavlang', function($name) {
+            return "<?php echo 'LaravelTopNav::ui.top_nav.'.$name === (\$trans = trans('LaravelTopNav::ui.top_nav.'.$name)) ? $name : \$trans ?>";
+        });
+
+        // include directive
         Blade::directive('laraveltopnav', function() {
             return Blade::compileString("@include('LaravelTopNav::menu', ['menu' => config('top_nav.menu')])");
         });
